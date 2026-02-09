227a5447b7a476e9a83e9c63ae89a07cc57a6d98
Kiran Poojary <64731026+kiranpoojari@users.noreply.github.com>
2026-02-09 20:04:37 +0530

    Update settings.py

--- agrihat_backend/settings.py
+++ agrihat_backend/settings.py
@@ -28,11 +28,7 @@ DEBUG = False
 # ALLOWED_HOSTS = []
 # DEBUG = False
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".onrender.com"
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
